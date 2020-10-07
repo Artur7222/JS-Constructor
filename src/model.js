8873fa4e236e1062ea03168b0498ddfd0755c48b
Ushakov Artur <ushakov.artur93@gmail.com>
2020-10-07 16:32:09 +0500

    первое сохранение

---
+++ src/model.js
@@ -0,0 +1,13 @@
+import image from './assets/js1.png';
+
+export const model = [
+    {type: 'title', value: 'Hello World from JS!!!!!'},
+    {type: 'text', value: 'Here we go with some text'},
+    {type: 'columns', value: [
+        '1111111',
+        '22222222',
+        '333333333',
+        '4444444'
+    ]},
+    {type: 'image', value: image}
+];
